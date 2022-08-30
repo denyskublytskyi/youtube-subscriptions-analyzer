456f81fcc34b1cb0a6f137ebb48915ebfb933d42
Denys Kublytskyi <deniskublitskiy@gmail.com>
2022-08-31 01:15:27 +0200

    :recycle: Use promise support in googleapis library

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { google } = require("googleapis");
 const chunk = require("lodash/chunk");
 const keyBy = require("lodash/keyBy");
 const groupBy = require("lodash/groupBy");
+const fpOrderBy = require("lodash/fp/orderBy");
+const compose = require("lodash/fp/compose");
 const countries = require("countries-code");
 
 const oauth2Client = new google.auth.OAuth2(
@@ -13,7 +15,7 @@ const oauth2Client = new google.auth.OAuth2(
   "http://localhost:3000"
 );
 
-const scopes = ["https://www.googleapis.com/auth/youtube"];
+const scopes = ["https://www.googleapis.com/auth/youtube", "profile"];
 
 const url = oauth2Client.generateAuthUrl({
   access_type: "offline",
@@ -33,24 +35,13 @@ const start = async () => {
   const subscriptions = [];
   do {
     // eslint-disable-next-line no-await-in-loop
-    const { data } = await new Promise((resolve, reject) => {
-      youtube.subscriptions.list(
-        {
-          part: "snippet",
-          mine: true,
-          maxResults: 50,
-          pageToken: nextPageToken,
-        },
-        // eslint-disable-next-line no-shadow
-        (err, data) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(data);
-        }
-      );
+    const { data } = await youtube.subscriptions.list({
+      part: "snippet",
+      mine: true,
+      maxResults: 50,
+      ...(nextPageToken && {
+        pageToken: nextPageToken,
+      }),
     });
 
     subscriptions.push(...data.items);
@@ -60,25 +51,14 @@ const start = async () => {
   const channels = (
     await Promise.all(
       chunk(subscriptions, 50).map(async (subscriptionsChunk) => {
-        const { data } = await new Promise((resolve, reject) => {
-          youtube.channels.list(
-            {
-              part: "snippet",
-              pageToken: nextPageToken,
-              id: subscriptionsChunk
-                .map(({ snippet }) => snippet.resourceId.channelId)
-                .join(","),
-            },
-            // eslint-disable-next-line no-shadow
-            (err, data) => {
-              if (err) {
-                reject(err);
-                return;
-              }
-
-              resolve(data);
-            }
-          );
+        const { data } = await youtube.channels.list({
+          part: "snippet",
+          id: subscriptionsChunk
+            .map(({ snippet }) => snippet.resourceId.channelId)
+            .join(","),
+          ...(nextPageToken && {
+            pageToken: nextPageToken,
+          }),
         });
 
         return data.items;
@@ -94,6 +74,8 @@ const start = async () => {
 
   // eslint-disable-next-line no-unused-vars
   const channelsByCountry = groupBy(channels, ({ snippet }) => snippet.country);
+  // eslint-disable-next-line no-console
+  console.log(channelsByCountry);
   const channelsPercentageByCountry = channels.reduce(
     (acc, { snippet }) => ({
       ...acc,
@@ -104,33 +86,24 @@ const start = async () => {
     }),
     {}
   );
-  // eslint-disable-next-line no-console
-  console.log(channelsPercentageByCountry);
 
   const ruChannelsSubscriptionsIds = ruChannels.map(
     ({ id }) => subscriptionsByChannelId[id].id
   );
 
   await Promise.all(
-    ruChannelsSubscriptionsIds.map(
-      (id) =>
-        new Promise((resolve, reject) => {
-          youtube.subscriptions.delete(
-            {
-              id,
-            },
-            (err, data) => {
-              if (err) {
-                reject(err);
-                return;
-              }
-
-              resolve(data);
-            }
-          );
-        })
+    ruChannelsSubscriptionsIds.map((id) =>
+      youtube.subscriptions.delete({
+        id,
+      })
     )
   );
+
+  return compose(
+    (entries) => Object.fromEntries(entries),
+    fpOrderBy(["1"], ["desc"]),
+    (obj) => Object.entries(obj)
+  )(channelsPercentageByCountry);
 };
 
 const server = http.createServer(async (req, res) => {
@@ -138,17 +111,26 @@ const server = http.createServer(async (req, res) => {
     "code"
   );
 
-  res.writeHead(200);
-  res.end("ok");
-
   if (!code) {
+    res.writeHead(200);
+    res.end("ok");
     return;
   }
 
   const { tokens } = await oauth2Client.getToken(code);
   oauth2Client.setCredentials(tokens);
 
-  await start();
+  const oauth2 = google.oauth2({
+    auth: oauth2Client,
+    version: "v2",
+  });
+  const { data } = await oauth2.userinfo.get();
+  // eslint-disable-next-line no-console
+  console.log(data);
+
+  const result = await start();
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(result));
 });
 
 server.listen(3000);
